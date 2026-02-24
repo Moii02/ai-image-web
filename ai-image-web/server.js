0d3cf6323c14870115320edd93e70afdb3ee81ba
Moii02 <siimoii2502@gmail.com>
2026-02-24 18:03:36 +0000

    #

--- ai-image-web/server.js
+++ ai-image-web/server.js
@@ -4,7 +4,7 @@ import fetch from "node-fetch";
 const app = express();
 app.use(express.json());
 
-const HF_API_KEY = "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";
+const HF_API_KEY = "MASUKKAN_API_KEY_KAMU";
 
 app.post("/generate", async (req, res) => {
   try {
